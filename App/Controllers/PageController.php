6569d53a84e5d6d59999fe89b7a1357c2234a4b9
Llanos07 <nicolas.llanos2709@gmail.com>
2023-11-22 22:30:31 -0300

    Estructuracion mvc y enrutamiento terminado

---
+++ App/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php 
+    class PageController{
+        public function home(){
+            echo "home";
+        }
+        public function create(){
+            echo "create";
+        }
+        public function read(){
+            echo "read";
+        }
+        public function update(){
+            echo "update";
+        }
+        public function delete(){
+            echo "delete";
+        }
+    }
