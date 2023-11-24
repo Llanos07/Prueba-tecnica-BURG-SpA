306d8eff8e54a23c8856ed933050995a4b11febb
Llanos07 <nicolas.llanos2709@gmail.com>
2023-11-23 22:34:24 -0300

    modificacion plain

--- App/Views/layouts/plain.layout.php
+++ App/Views/layouts/plain.layout.php
@@ -20,6 +20,20 @@ if (!isset($_SESSION['user'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand">BURG SpA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item" id="homeNav"><a class="nav-link"  href="../page/login">Login</a></li>
+                <li class="nav-item" id="profileNav"><a class="nav-link"  href="../page/create">Registrarse</a></li>
+            </ul>
+            </div>
+        </div>
+    </nav>
 
     <?php echo $content ?>
     
